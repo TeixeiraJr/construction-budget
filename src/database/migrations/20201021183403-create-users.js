3a05a77f76edd8ac14135d7f247f1c22f6f5b4e9
Junior <teixeirajunior1710@gmail.com>
2021-06-11 16:42:54 -0300

    migration de criar usuario

--- src/database/migrations/20201021183403-create-users.js
+++ src/database/migrations/20201021183403-create-users.js
@@ -23,6 +23,11 @@ module.exports = {
         allowNull: false
       },
 
+      shopName: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+
       tokenGoogle: {
         type: Sequelize.TEXT,
         unique: true,
@@ -45,24 +50,44 @@ module.exports = {
         allowNull: true
       },
 
+      cpfCnpj: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+
       phone: {
         type: Sequelize.STRING,
         allowNull: true
       },
 
-      avatar: {
+      address: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+
+      addressNumber: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+
+      postalCode: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+
+      logo: {
         type: Sequelize.TEXT,
         allowNull: true
+      },
+
+      type: {
+        type: Sequelize.ENUM(['admin', 'support', 'user']),
+        allowNull: false
       },
 
       birthday: {
         type: Sequelize.DATEONLY,
         allowNull: true
-      },
-
-      type: {
-        type: Sequelize.ENUM(['admin', 'teacher', 'student']),
-        allowNull: false
       },
 
       createdAt: {
